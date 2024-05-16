a79fed15c3ecfcc8d846083aff0b33863497109c
Chanathip Khamchan <72792764+khoy2589@users.noreply.github.com>
2024-05-16 11:34:53 +0700

    Titanium texture Initialized

--- src/main/java/net/banana/tutorialmod/TutorialMod.java
+++ src/main/java/net/banana/tutorialmod/TutorialMod.java
@@ -1,15 +1,26 @@
 package net.banana.tutorialmod;
 
+import net.banana.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 	}
+
+
 }
+
---
+++ src/main/java/net/banana/tutorialmod/item/ModItems.java
@@ -0,0 +1,30 @@
+package net.banana.tutorialmod.item;
+
+import net.banana.tutorialmod.TutorialMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+
+public class ModItems {
+    public static final Item TITANIUM = registerItem("titanium", new Item(new FabricItemSettings()));
+
+    private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries){
+        entries.add(TITANIUM);
+    }
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, new Identifier(TutorialMod.MOD_ID, name),item);
+    }
+
+    public static void registerModItems(){
+        TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientItemGroup);
+    }
+}
